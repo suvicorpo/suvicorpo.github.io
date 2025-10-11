a0bdce4d4d91f5f14f72c6bf63003c96163bc341
Pivonian <suvicorp@hotmail.com>
2025-10-11 20:49:03 +1000

    import getDoc

--- js/firebase-auth.js
+++ js/firebase-auth.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 import { firebaseConfig } from "./firebase-conf.js"
 
 const app = initializeApp(firebaseConfig);
