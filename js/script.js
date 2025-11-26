15cf966fbf6bcd6d7af51df2d99bdcd19bb9d604
KettleLord <bodhireimer@gmail.com>
2025-11-26 12:13:26 +1000

    move contant js to script section of contact page

--- js/script.js
+++ js/script.js
@@ -10,26 +10,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-document.getElementById('contact').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  const form = e.target;
-  const data = new FormData(form);
-
-  fetch(form.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      'Accept': 'application/json'
-    }
-  }).then(response => {
-    if (response.ok) {
-      alert('Thanks for reaching out!');
-      form.reset();
-    } else {
-      alert('Oops! There was a problem submitting your form.');
-    }
-  }).catch(error => {
-    alert('Oops! There was a problem submitting your form.');
-  });
-});
